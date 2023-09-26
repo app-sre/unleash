27ec80eb078f8d39cae2beab9a922ba347d7a0cf
Gerd Oberlechner <goberlec@redhat.com>
2023-09-26 10:47:56 +0200

    PKO bundling for unleash (#49)
    introduces PKO package bundling and publishing for unleash
    
    part of https://issues.redhat.com/browse/APPSRE-8134

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 IMAGE_NAME="quay.io/app-sre/unleash"
+PKO_IMAGE_NAME="quay.io/app-sre/unleash-pko"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
+# build and push unleash image
 docker build -t "${IMAGE_NAME}:latest" .
 docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
 
@@ -12,3 +14,11 @@ docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay
 
 docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:latest"
 docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:${IMAGE_TAG}"
+
+# build and push PKO image
+cp -R $PWD/pko/package/ ${PWD}/pko/tmp
+docker run -v .:/unleash:z quay.io/app-sre/ubi8-ubi-minimal:8.8 sed -i "s/UNLEASH_IMAGE_TAG/$IMAGE_TAG/g" /unleash/pko/tmp/manifest.yaml
+docker run -v .:/unleash:z quay.io/app-sre/package-operator-cli:92430cd build -t "${PKO_IMAGE_NAME}:${IMAGE_TAG}" -t "${PKO_IMAGE_NAME}:latest" /unleash/pko/tmp -o /unleash/pko/image.tgz
+docker load < pko/image.tgz
+docker --config="${DOCKER_CONF}" push "${PKO_IMAGE_NAME}:latest"
+docker --config="${DOCKER_CONF}" push "${PKO_IMAGE_NAME}:${IMAGE_TAG}"
--- pr_check.sh
+++ pr_check.sh
@@ -4,3 +4,8 @@ IMAGE_TEST=unleash-test
 
 # Let's just build the image. That's enough test for now.
 docker build -t ${IMAGE_TEST} -f Dockerfile .
+
+# Validate package
+docker run -v .:/unleash:z quay.io/app-sre/package-operator-cli:92430cd validate /unleash/pko/package
+docker run -v .:/unleash:z quay.io/app-sre/package-operator-cli:92430cd tree /unleash/pko/package --config-path /unleash/pko/validation_config.yaml
+# todo - use test feature
