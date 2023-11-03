3462b1bbb82d35cb6b5bfefd8101fd4cf761969f
Di Wang <dwan@redhat.com>
2023-11-03 16:05:41 +1100

    Use k6 quay mirror to build integration_test
    Signed-off-by: Di Wang <dwan@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,13 +3,14 @@
 IMAGE_NAME="quay.io/app-sre/unleash"
 PKO_IMAGE_NAME="quay.io/app-sre/unleash-pko"
 INTEGRATION_TEST_IMAGE_TAG_PREFIX="integration-test-"
+K6_IMAGE="quay.io/app-sre/k6"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 # build and push unleash image
 docker build -t "${IMAGE_NAME}:latest" .
 docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
 
-docker build -t "${IMAGE_NAME}:${INTEGRATION_TEST_IMAGE_TAG_PREFIX}${IMAGE_TAG}" -f integration_test/Dockerfile ./integration_test
+docker build -t "${IMAGE_NAME}:${INTEGRATION_TEST_IMAGE_TAG_PREFIX}${IMAGE_TAG}" -f integration_test/Dockerfile --build-arg="K6_IMAGE=$K6_IMAGE" ./integration_test
 
 DOCKER_CONF="${PWD}/.docker"
 mkdir -p "${DOCKER_CONF}"
