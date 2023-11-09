619f83bd049bc693d03587cc7bb5c7255fbbf289
Di Wang <dwan@redhat.com>
2023-11-09 20:15:11 +1100

    Update pr_check script to run tests
    Signed-off-by: Di Wang <dwan@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -7,10 +7,14 @@ IMAGE_INTEGRATION_TEST=unleash-integration-test
 K6_IMAGE="quay.io/app-sre/k6"
 PKO_BUILD_TEST=pko-build-test
 
-# Let's just build the image. That's enough test for now.
+docker build --target dev -t ${IMAGE_TEST} -f Dockerfile .
+docker run --rm ${IMAGE_TEST} npm run lint
+docker run --rm ${IMAGE_TEST} npm run test
+
+# Validate pko package
+docker build --target test -t ${PKO_BUILD_TEST} -f pko/Dockerfile ./pko
+
+# Test build final images
 docker build -t ${IMAGE_TEST} -f Dockerfile .
 docker build -t ${IMAGE_INTEGRATION_TEST} -f integration_test/Dockerfile --build-arg="K6_IMAGE=$K6_IMAGE" ./integration_test
-
-# Validate package
-docker build --target test -t ${PKO_BUILD_TEST} -f pko/Dockerfile ./pko
-# todo - use test feature
+docker build -t ${PKO_BUILD_TEST} -f pko/Dockerfile ./pko
